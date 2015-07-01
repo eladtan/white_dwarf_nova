18a07f6b1f831c97aa9c4ff25a141814f8f6820e
Almog Yalinewich <almog.yalin@gmail.com>
2015-07-01 17:06:06 +0300

    create visualisation script

---
+++ draw_snapshots.py
@@ -0,0 +1,193 @@
+def plot_single(in_file, zfunc, zname, out_file):
+
+    import pylab
+    import numpy
+    import h5py
+    from matplotlib.collections import PolyCollection
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    import os
+
+    print(out_file)
+    if os.path.isfile(out_file):
+        return
+
+    with h5py.File(in_file,'r+') as f:
+        vert_idx_list = numpy.concatenate(([0],
+                                           numpy.cumsum(f['Number of vertices in cell'])))
+        verts = []
+        for i in range(len(f['density'])):
+            if f['ghost'][i]<0.5:
+                lowbound = int(vert_idx_list[i])
+                upbound = int(vert_idx_list[i+1])
+                verts.append([[x,y] for x,y
+                              in zip(f['x position of vertices'][lowbound:upbound],
+                                     f['y position of vertices'][lowbound:upbound])])
+        coll = PolyCollection(verts, 
+                              array=zfunc(f),
+                              cmap = mpl.cm.jet,
+                              edgecolors = 'none')
+        fig, ax = plt.subplots()
+        fig.suptitle(zname+' @ t = '+str(numpy.array(f['time'])[0]))
+        ax.add_collection(coll)
+        ax.autoscale_view()
+        ax.set_aspect('equal')
+        fig.colorbar(coll,ax=ax)
+        print(out_file)
+        if out_file==None:
+            plt.show()
+        else:
+            plt.savefig(out_file)
+
+def plot_all(zfunc, zname):
+
+    import glob
+    import numpy
+    import joblib
+
+    flist = glob.glob('snapshot_*.h5')
+
+    joblib.Parallel(n_jobs=8)(joblib.delayed(plot_single)
+                              (fname,
+                               zfunc,
+                               zname,
+                               fname.replace('snapshot',zname).replace('.h5','.png')) for fname in flist)
+    #[plot_single(fname,zfunc,zname,
+    #             fname.replace('snapshot',zname).replace('.h5','.png'))
+    # for fname in flist]
+
+def log10_number_density_cgs(f):
+
+    import numpy
+
+    return numpy.log10(numpy.array(f['density']))[numpy.array(f['ghost'])<0.5]
+
+def log10_temperature(f):
+
+    import numpy
+
+    return numpy.log10(f['temperature'])[numpy.array(f['ghost'])<0.5]
+
+def x_velocity(f):
+
+    import numpy
+
+    return numpy.array(f['x_velocity'])[numpy.array(f['ghost'])<0.5]
+
+def y_velocity(f):
+
+    import numpy
+
+    return numpy.array(f['y_velocity'])[numpy.array(f['ghost'])<0.5]
+
+def He4_prof(f):
+
+    import numpy
+
+    return numpy.array(f['He4'])[numpy.array(f['ghost'])<0.5]
+
+def C12_prof(f):
+
+    import numpy
+
+    return numpy.array(f['C12'])[numpy.array(f['ghost'])<0.5]
+
+def O16_prof(f):
+
+    import numpy
+
+    return numpy.array(f['O16'])[numpy.array(f['ghost'])<0.5]
+
+def Ne20_prof(f):
+
+    import numpy
+
+    return numpy.array(f['Ne20'])[numpy.array(f['ghost'])<0.5]
+
+def Mg24_prof(f):
+
+    import numpy
+
+    return numpy.array(f['Mg24'])[numpy.array(f['ghost'])<0.5]
+
+def Si28_prof(f):
+
+    import numpy
+
+    return numpy.array(f['Si28'])[numpy.array(f['ghost'])<0.5]
+
+def S32_prof(f):
+
+    import numpy
+
+    return numpy.array(f['S32'])[numpy.array(f['ghost'])<0.5]
+
+def Ar36_prof(f):
+
+    import numpy
+
+    return numpy.array(f['Ar36'])[numpy.array(f['ghost'])<0.5]
+
+def Ca40_prof(f):
+
+    import numpy
+
+    return numpy.array(f['Ca40'])[numpy.array(f['ghost'])<0.5]
+
+def Ti44_prof(f):
+
+    import numpy
+
+    return numpy.array(f['Ti44'])[numpy.array(f['ghost'])<0.5]
+
+def Cr48_prof(f):
+
+    import numpy
+
+    return numpy.array(f['Cr48'])[numpy.array(f['ghost'])<0.5]
+
+def Fe52_prof(f):
+
+    import numpy
+
+    return numpy.array(f['Fe52'])[numpy.array(f['ghost'])<0.5]
+
+def Ni56_prof(f):
+
+    import numpy
+
+    return numpy.array(f['Ni56'])[numpy.array(f['ghost'])<0.5]
+
+def main():
+
+    import matplotlib
+    matplotlib.use('Qt4Agg')
+
+    import numpy
+
+    #plot_single('snapshot_0.h5',
+    #            log10_temperature,
+    #            'log10_temperature',
+    #            'log10_temperature_0.png')
+
+    plot_all(log10_number_density_cgs, 'log10_number_density')
+    plot_all(log10_temperature, 'log10_temperature')
+    plot_all(x_velocity, 'x_velocity')
+    plot_all(y_velocity, 'y_velocity')
+    #plot_all(He4_prof, 'He4')
+    #plot_all(C12_prof, 'C12')
+    #plot_all(O16_prof, 'O16')
+    #plot_all(S32_prof, 'S32')
+    #plot_all(Ne20_prof, 'Ne20')
+    #plot_all(Mg24_prof, 'Mg24')
+    #plot_all(Si28_prof, 'Si28')
+    #plot_all(Ar36_prof, 'Ar36')
+    #plot_all(Ca40_prof, 'Ca40')
+    #plot_all(Ti44_prof, 'Ti44')
+    #plot_all(Cr48_prof, 'Cr48')
+    #plot_all(Fe52_prof, 'Fe52')
+    #plot_all(Ni56_prof, 'Ni56')
+
+if __name__ == '__main__':
+
+    main()
