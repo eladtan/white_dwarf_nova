32c41cb42ae2e7c243d0b9ad12d1a966001d68f3
Almog Yalinewich <almog.yalin@gmail.com>
2015-07-04 09:41:15 +0300

    add feature to pushpullet script: send videos

--- announce_via_pushbullet.py
+++ announce_via_pushbullet.py
@@ -2,14 +2,18 @@ def main():
 
     from yapbl import PushBullet
     import os
+    import glob
 
     assert(os.path.isfile('access_token.txt'))
     with open('access_token.txt') as f:
         access_token = f.readline().rstrip()
 
-    print(access_token)
-
     p = PushBullet(access_token)
+
+    mp4_list = glob.glob('*.mp4')
+    if len(mp4_list)>0:
+        for fname in mp4_list:
+            p.push_file(open(fname,'rb'))
     p.push_note('the simulation is finished',
                 os.getcwd())
 
