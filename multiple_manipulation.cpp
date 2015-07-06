f62f47de808860e213da8806c0e192cb18c07716
Almog Yalinewich <almog.yalin@gmail.com>
2015-07-06 06:15:09 +0300

    add multiple diagnostics

---
+++ multiple_manipulation.cpp
@@ -0,0 +1,17 @@
+#include "multiple_manipulation.hpp"
+
+MultipleManipulation::MultipleManipulation
+(const vector<Manipulate*>& manip_list):
+  manip_list_(manip_list) {}
+
+void MultipleManipulation::operator()(hdsim& sim)
+{
+  for(size_t i=0;i<manip_list_.size();++i)
+    (*manip_list_[i])(sim);
+}
+
+MultipleManipulation::~MultipleManipulation(void)
+{
+  for(size_t i=0;i<manip_list_.size();++i)
+    delete manip_list_[i];
+}
---
+++ multiple_manipulation.hpp
@@ -0,0 +1,21 @@
+#ifndef MULTIPLE_MANIPULATION_HPP
+#define MULTIPLE_MANIPULATION_HPP 1
+
+#include "source/newtonian/test_2d/main_loop_2d.hpp"
+
+class MultipleManipulation: public Manipulate
+{
+public:
+
+  MultipleManipulation(const vector<Manipulate*>& manip_list);
+
+  void operator()(hdsim& sim);
+
+  ~MultipleManipulation(void);
+
+private:
+
+  const vector<Manipulate*> manip_list_;
+};
+
+#endif // MULTIPLE_MANIPULATION_HPP
