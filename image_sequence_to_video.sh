1c3213c251c7a418f82e73c6a3b54c831bea60b3
Almog Yalinewich <almog.yalin@gmail.com>
2015-08-06 13:35:26 +0300

    optimise visualisation

--- image_sequence_to_video.sh
+++ image_sequence_to_video.sh
@@ -1,4 +1,4 @@
-rm *.mp4
+rm *.webm
 ffmpeg -i log10_density_%d.png -codec:v libvpx "wd_nova_log10_density_`git rev-parse HEAD`.webm"
 ffmpeg -i log10_temperature_%d.png -codec:v libvpx "wd_nova_log10_temperature_`git rev-parse HEAD`.webm"
 ffmpeg -i x_velocity_%d.png -codec:v libvpx "wd_nova_x_velocity_`git rev-parse HEAD`.webm"
