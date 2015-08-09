6f66ff83d290decd43b79a5fe022c8668be82ce7
Almog Yalinewich <almog.yalin@gmail.com>
2015-08-09 11:53:10 +0300

    create feature to write the running wall time to file

--- rich.cpp
+++ rich.cpp
@@ -1,3 +1,4 @@
+#include <ctime>
 #include "units.hpp"
 #include "sim_data.hpp"
 #include "my_main_loop.hpp"
@@ -6,6 +7,7 @@ using namespace std;
 
 int main(void)
 {
+  const clock_t begin = clock();
   const Units units;
   const InitialData id
     ("radius_list.txt",
@@ -20,5 +22,11 @@ int main(void)
 				   0.54*M_PI));
   hdsim& sim = sim_data.getSim();
   my_main_loop(sim,sim_data.getEOS());
+
+  const clock_t end = clock();
+  ofstream f("wall_time.txt");
+  f << static_cast<double>(end-begin)/CLOCKS_PER_SEC << endl;
+  f.close();
+
   return 0;
 }
